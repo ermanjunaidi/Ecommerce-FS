79ad601e3da18240a1fb5790046a0798cf5a4aa9
ermanjunaidi <ermanjunaidi1@gmail.com>
2023-08-20 16:30:34 +0700

    sini dulu

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("helo");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`server berjalan di port ${port}`);
+});
